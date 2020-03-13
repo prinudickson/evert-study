022e232286ca7762662ceb3ed19e5ce4915994bd
Prinu Dickson <prinudickson@gmail.com>
2020-03-13 16:33:48 +0100

    started working on the script.

---
+++ R-Script/prinu_script.R
@@ -0,0 +1,15 @@
+#Install the required packages----
+install.packages("readr")
+install.packages("dplyr")
+install.packages("tidyr")
+
+#Load the required packages----
+library(readr)
+library(dplyr)
+library(tidyr)
+
+#working directory----
+getwd()
+
+#Set this based on your laptop's working directory
+setwd("C:/Users/pdickson004/Desktop/Personal_Projects/evert-study")
